61e6f77b5a32acbd594c21b0df65f2643aa899a4
sincekas <salimcanincekas@gmail.com>
2022-11-17 01:46:04 +0300

    deneme

--- day1/main.py
+++ day1/main.py
@@ -1,25 +1,17 @@
-baslik = "HABERINIZ OLSUN"
+dolar_dun = 7.65
+dolar_bugun = 7.75
 
-vade = 12
-faiz_orani1 = 1.47
-faiz_orani2 = 1.44
+if dolar_dun>dolar_bugun:
+    print('Azalis oku')
+    print('Bitti')
+elif dolar_dun<dolar_bugun:
+    print('Artis oku')
+else:
+    print('Esittir oku')
 
 
-print(baslik)
-print(type((baslik)))
-print(type((vade)))
-print(type((faiz_orani1)))
-print(type((faiz_orani2)))
+#if dolar_dun<dolar_bugun:
+    print('Artis oku')
 
-mesaj = "Hoşgeldin"
-musteri_adi = "Salimcan"
-musteri_soyadi = "İncekaş"
-
-print(mesaj+" "+musteri_adi+" "+musteri_soyadi+"!")
-sonuc_mesaj = f"{mesaj} {musteri_adi} {musteri_soyadi}!"
-print((sonuc_mesaj))
-
-sayi1 = 10
-sayi2 = 20
-print(sayi1+sayi2)
-
+#if dolar_dun==dolar_bugun:
+    print('Esittir ooku')
