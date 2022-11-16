3bdf9ec17d00a4c0c7d675f4cebb2f6c08f41079
sincekas <salimcanincekas@gmail.com>
2022-11-17 01:30:12 +0300

    first commit

---
+++ day1/main.py
@@ -0,0 +1,25 @@
+baslik = "HABERINIZ OLSUN"
+
+vade = 12
+faiz_orani1 = 1.47
+faiz_orani2 = 1.44
+
+
+print(baslik)
+print(type((baslik)))
+print(type((vade)))
+print(type((faiz_orani1)))
+print(type((faiz_orani2)))
+
+mesaj = "Hoşgeldin"
+musteri_adi = "Salimcan"
+musteri_soyadi = "İncekaş"
+
+print(mesaj+" "+musteri_adi+" "+musteri_soyadi+"!")
+sonuc_mesaj = f"{mesaj} {musteri_adi} {musteri_soyadi}!"
+print((sonuc_mesaj))
+
+sayi1 = 10
+sayi2 = 20
+print(sayi1+sayi2)
+
