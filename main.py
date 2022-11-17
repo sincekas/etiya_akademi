e6d99527432b6b03b5f3d5a246fa5623a6b13bbd
sincekas <salimcanincekas@gmail.com>
2022-11-17 11:58:21 +0300

    last commit

---
+++ main.py
@@ -0,0 +1,44 @@
+print('Merhaba Etiya')
+
+text = '10'
+print('text')
+
+number = 10
+print(number)
+
+dnumber = 5.3
+print(dnumber)
+
+print(number + 5)
+print(number - 5)
+print(number * 5)
+print(number / 5)
+print(number % 5)
+
+is_verified = True
+
+print(1 == 2) #f
+print(2 != 2) #f
+
+print(2 > 2) #f
+print(2 < 2) #f
+
+print(10%2 == 0) #t
+
+print(50/2 == 25) # t
+print(50/2 == 25.0) #t
+
+text = 'Merhaba Etiya'
+# print(text.upper()) # MERHABA ETİYA
+# print(text.lower()) # merhaba etiya
+# print(text.startswith('Mer')) #t
+print(text.endswith('Etiya')) #t
+
+name = 'Salim'
+age = 24
+company = 'Etiya'
+
+# Salim 24 yaşında Etiya'da çalışıyor.
+print(f"{name} {age} yaşında, {company}'da çalışıyor. ")
+
+print('Merhaba github')
